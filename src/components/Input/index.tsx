77d0a3e24e0c186f541177eb6fda1819a27a3869
biagavirete <biagavirete@gmail.com>
2020-09-03 17:13:52 -0300

    show error in input

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -43,6 +43,8 @@ const Input: React.FC<InputProps> = ({ name, icon: Icon, ...rest }) => {
       onBlur={handleInputBlur}
       defaultValue={defaultValue}
       ref={inputRef} {...rest} />
+
+      {error}
     </Container>
   );
 };
