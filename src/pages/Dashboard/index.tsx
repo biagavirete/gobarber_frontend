fc5a5cdf5c249d6eea7516d90556c52d3b33f0ba
biagavirete <biagavirete@gmail.com>
2020-10-06 14:19:26 -0300

    style dashboard appointments list

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,12 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-import { Container, Header, HeaderContent, Profile } from './styles';
+import { Container, Header, HeaderContent, Profile, Content, Schedule, Calendar, NextAppointment, Section, Appointment } from './styles';
 
 import logoImg from '../../assets/logo.svg';
-import { FiPower } from 'react-icons/fi';
+import { FiClock, FiPower } from 'react-icons/fi';
 import { useAuth } from '../../hooks/auth';
 
 const Dashboard: React.FC = () => {
+  const [selectedDate, setSelectedDate] = useState(new Date());
+
   const { signOut, user } = useAuth();
 
   return(
@@ -28,6 +30,78 @@ const Dashboard: React.FC = () => {
       </button>
       </HeaderContent>
     </Header>
+
+    <Content>
+      <Schedule>
+        <h1>Horários agendados</h1>
+        <p>
+          <span>Hoje</span>
+          <span>Dia 06</span>
+          <span>Segunda-feira</span>
+        </p>
+
+        <NextAppointment>
+          <strong>Atendimento a seguir</strong>
+          <div>
+            <img src="https://avatars1.githubusercontent.com/u/60673841?s=460&u=3b00d64b64735db0e1c3f20d1010eddcd62a7837&v=4" alt="Gabriel Asorey"/>
+
+            <strong>Gabriel Asorey</strong>
+            <span>
+              <FiClock />08:00
+            </span>
+          </div>
+        </NextAppointment>
+
+        <Section>
+          <strong>Manhã</strong>
+
+          <Appointment>
+            <span>
+              <FiClock />
+              08:00
+            </span>
+
+            <div>
+              <img src="https://avatars1.githubusercontent.com/u/60673841?s=460&u=3b00d64b64735db0e1c3f20d1010eddcd62a7837&v=4" alt="Gabriel Asorey"/>
+              <strong>Gabriel Asorey</strong>
+            </div>
+          </Appointment>
+
+          <Appointment>
+            <span>
+              <FiClock />
+              08:00
+            </span>
+
+            <div>
+              <img src="https://avatars1.githubusercontent.com/u/60673841?s=460&u=3b00d64b64735db0e1c3f20d1010eddcd62a7837&v=4" alt="Gabriel Asorey"/>
+              <strong>Gabriel Asorey</strong>
+            </div>
+          </Appointment>
+        </Section>
+
+        <Section>
+          <strong>Tarde</strong>
+
+          <Appointment>
+            <span>
+              <FiClock />
+              08:00
+            </span>
+
+            <div>
+              <img src="https://avatars1.githubusercontent.com/u/60673841?s=460&u=3b00d64b64735db0e1c3f20d1010eddcd62a7837&v=4" alt="Gabriel Asorey"/>
+              <strong>Gabriel Asorey</strong>
+            </div>
+          </Appointment>
+        </Section>
+
+      </Schedule>
+
+      <Calendar>
+
+        </Calendar>
+    </Content>
 
   </Container>
   )
